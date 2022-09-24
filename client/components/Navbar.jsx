005699df514fbbe6fb03021f16886492c5113696
Vincent Tiu <46623413+vincetiu8@users.noreply.github.com>
2022-09-24 11:02:55 -0400

    Final changes (#9)
    * TESTING
    
    TESTING
    
    * mh
    
    sds
    
    * edits
    
    Co-authored-by: Brilynd <86674962+Brilynd@users.noreply.github.com>

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,32 +1,21 @@
 const Navbar = ({}) => {
 	return (
-		<nav class="px-2 bg-white border-gray-200 dark:bg-green-600 dark:border-green-600">
+		<nav class="p-2 bg-white border-gray-200 dark:bg-green-600 dark:border-green-600">
 			<div class="container flex flex-wrap justify-between items-center mx-auto">
 				<a href="/" class="flex items-center">
 					<img src="/images/blossom-logo.png" class="mr-3 h-6 sm:h-10 color-white"
 					     style={{filter: "brightness(100)"}} alt="Flowbite Logo"/>
 					<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Blossom</span>
 				</a>
-				<button data-collapse-toggle="mobile-menu" type="button"
-				        class="inline-flex justify-center items-center ml-3 text-gray-400 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500"
-				        aria-controls="mobile-menu-2" aria-expanded="false">
-					<span class="sr-only">Open main menu</span>
-					<svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
-					     xmlns="http://www.w3.org/2000/svg">
-						<path fill-rule="evenodd"
-						      d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-						      clip-rule="evenodd"></path>
-					</svg>
-				</button>
-				<div class="hidden w-full md:block md:w-auto" id="mobile-menu">
-					<ul class="flex flex-col p-4 mt-4 bg-green-600 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:dark:bg-green-600 dark:bg-green-600">
-						<li>
-							<a href="/locations"
-							   class="block py-2 pr-4 pl-3 text-white rounded hover:bg-white md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-white dark:hover:text-white md:dark:hover:bg-transparent">Plan
-								a date</a>
-						</li>
-					</ul>
-				</div>
+
+				<a href="#"
+				   class="block py-2 pr-4 pl-3 text-white rounded hover:bg-white md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-white dark:hover:text-white md:dark:hover:bg-transparent">Meet</a>
+
+				<a href="#"
+				   className="block py-2 pr-4 pl-3 text-white rounded hover:bg-white md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-white dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
+
+				<a href="/locations"
+				   class="block py-2 pr-4 pl-3 text-white rounded hover:bg-white md:hover:bg-transparent md:border-0 md:hover:text-white md:p-0 dark:text-white md:dark:hover:text-white dark:hover:bg-white dark:hover:text-white md:dark:hover:bg-transparent">Plan a date</a>
 			</div>
 		</nav>
 	)
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,55 +1,66 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import nextJsPic from "../public/images/nextjs.png"
-import golangPic from "../public/images/golang.png"
-import Navbar from '../components/Navbar'
+import {useRouter} from "next/router";
+import axios from "axios";
 
 export default function Home() {
-	return (
-		<div>
-			<div className="Jumbotron-Overlay">Plan a green date.</div>
-			<section class="overflow-hidden bg-green-100 ">
-				<div class="container py-2 mx-auto lg:pt-12 lg:px-32">
-					<div class="flex flex-wrap -m-1 md:-m-2">
-						<div class="flex flex-wrap w-1/3">
-							<div class="w-full p-1 md:p-2">
-								<img alt="gallery" class="block object-cover object-center w-full h-full "
-								     src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"/>
-							</div>
-						</div>
-						<div class="flex flex-wrap w-1/3">
-							<div class="w-full p-1 md:p-2">
-								<img alt="gallery" class="block object-cover object-center w-full h-full "
-								     src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"/>
-							</div>
-						</div>
-						<div class="flex flex-wrap w-1/3">
-							<div class="w-full p-1 md:p-2">
-								<img alt="gallery" class="block object-cover object-center w-full h-full "
-								     src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"/>
-							</div>
-						</div>
-						<div class="flex flex-wrap w-1/3">
-							<div class="w-full p-1 md:p-2">
-								<img alt="gallery" class="block object-cover object-center w-full h-full "
-								     src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"/>
-							</div>
-						</div>
-						<div class="flex flex-wrap w-1/3">
-							<div class="w-full p-1 md:p-2">
-								<img alt="gallery" class="block object-cover object-center w-full h-full "
-								     src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(76).webp"/>
-							</div>
-						</div>
-						<div class="flex flex-wrap w-1/3">
-							<div class="w-full p-1 md:p-2">
-								<img alt="gallery" class="block object-cover object-center w-full h-full "
-								     src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"/>
-							</div>
-						</div>
+	const router = useRouter()
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		const params = new URLSearchParams();
+		params.append('To', e.target.elements.phone_number.value);
+		params.append('MessagingServiceSid', 'MG74760c00bc6a755f4d4e57fbcfe86943');
+		params.append('Body', 'Hi there, thanks for signing up with Blossom! We think you\'d really like this local eco-friendly date idea: http://35.231.183.210/picnic. Check it out, and find many other sustainable dating ideas on our website!');
+		await axios.post(
+			"https://api.twilio.com/2010-04-01/Accounts/AC70589c2962e39a1213c0ca015a988912/Messages.json", params, {
+				auth: {
+					username:  'AC70589c2962e39a1213c0ca015a988912',
+					password: '1003a07a763d76be1a7d94c09c479483'
+				}
+			}
+		)
+		await router.push('/locations');
+	}
+
+	return (<div>
+			<div className="min-h-screen flex flex-col text-gray-600 container-testing">
+				<main className="container mx-auto px-6 pt-16 flex-1 text-center">
+					<h2 className="text-2xl md:text-4xl lg:text-6xl uppercase">Welcome to</h2>
+					<h1 className="text-3xl md:text-6xl lg:text-8xl uppercase font-black mb-8">Blossom</h1>
+
+					<p className="text-base md:text-lg lg:text-2xl mb-8">Let your love blossom as the earth blossoms</p>
+
+					<div
+						className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-90 w-fit mx-auto mb-8 rounded-full">
+						943,245 members
 					</div>
-				</div>
-			</section>
+
+					<form
+						method="post"
+						id="revue-form"
+						name="revue-form"
+						target="_blank"
+						onSubmit={onSubmit}
+					>
+
+						<div className="flex flex-col md:flex-row justify-center mb-4">
+							<input
+								placeholder="Your phone number..."
+								type="text"
+								name="phone_number"
+								id="member_email"
+								className="text-2xl placeholder:text-gray-400 placeholder:italic py-4 px-6 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-90 focus:bg-opacity-100 duration-150 md:rounded-tr-none md:rounded-br-none rounded-full outline-none mb-4 md:mb-0"
+							/>
+							<input
+								type="submit"
+								value="Join Today"
+								name="member[subscribe]"
+								id="member_submit"
+								className="bg-primary bg-white  opacity-90 md:rounded-tl-none md:rounded-bl-none rounded-full text-2xl py-4 px-6 md:px-10 lg:py-6 lg:px-12 font-bold uppercase cursor-pointer hover:opacity-100 duration-150"
+							/>
+						</div>
+					</form>
+				</main>
+			</div>
 		</div>
-	)
+	);
 }
